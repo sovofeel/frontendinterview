ce4b6a99f6a34d1e140157167c0bc458f997c897
sovofeel <sovofeel@gmail.com>
2021-03-07 18:28:16 +0500

    feat(all): add initial deps

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ChakraProvider } from "@chakra-ui/react"
+import type { AppProps  } from 'next/app'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider>
+       <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default App
